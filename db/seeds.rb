114e5d489f82cc8dd590b4414ce3bb8e332bd4ed
Antoine Brisson <brissux.antoine@gmail.com>
2024-05-22 17:05:35 +0200

    finito

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Creating restaurants..."
+6.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanaese french belgian].sample
+  )
+  puts "Restaurant #{restaurant.name} created!"
+end
+
+puts "Finished!"
